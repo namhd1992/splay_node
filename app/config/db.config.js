77d4a032b5e20dbe8c094f1e5c485c1b8a042318
namhd1992 <stpnghd.vn@gmail.com>
2024-02-23 14:43:50 +0700

    change ip

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // HOST: "103.82.28.173",
-  HOST:"171.244.11.173",
+  HOST: "103.82.28.173",
+  // HOST:"171.244.11.173",
   USER: "datlq",
   PASSWORD: "Vtcmobile!@#",
   DB: "sandbox_vtc_scoin_v2"
